d746d905f29b372f3c3814eee0233e385b385ffe
Tony <kanyianthony79@gmail.com>
2024-08-10 11:20:14 +0300

    first commit

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+// Your code here
+export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Invalid input");
+    }
+    const sanitizedWord = word.toLowerCase();
+    return sanitizedWord === reverseString(sanitizedWord);
+  }
+  
+  function reverseString(str) {
+    return [...str].reverse().join("");
+  }
